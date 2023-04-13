b0b8da33f911629773d53823fb23a1bac7d8b95e
Paolo Anedda <paolo.anedda@gmail.com>
2023-04-13 17:56:11 +0200

    Events record added
    The Event record data structure has been added as well as some functions to create it starting from a log line

--- src/clolog/events.clj
+++ src/clolog/events.clj
@@ -14,3 +14,12 @@
     (->Event (nth values 0) (nth values 1) (nth values 2) (nth values 3) (nth values 4)))
    )
   )
+
+(defn create-event-list-from-multiple-lines
+  "Creates an Event record from a comma separated line"
+  ([lines]
+  (let [values (split-lines lines)]
+    (map create-event-from-line values)
+   )
+  )
+)
--- test/clolog/core_test.clj
+++ test/clolog/core_test.clj
@@ -46,7 +46,38 @@ field5,field6")]
     )
   )
 
+(defn compare-create-event-from-line-results
+  "This is to compare the results of the Event record creation"
+  [event-record event-line]
+  (let [line-tokens (split-values event-line)]
+    (if (and (= (:timestamp event-record) (nth line-tokens 0))
+             (= (:event-id event-record) (nth line-tokens 1))
+             (= (:activity event-record) (nth line-tokens 2))
+             (= (:actor event-record) (nth line-tokens 3))
+             (= (:event-data event-record) (nth line-tokens 4)))
+      (identity true)
+      (identity false)
+      )
+    )
+  )
+
 (deftest test-create-event-from-line
   (testing "Testing the creation of an Event from a line"
-    (is (instance? clolog.events.Event (create-event-from-line "09/04/2023,000001,activity,joe,event_data"))))
+    (let [event-line "09/04/2023,000001,activity,joe,event_data,1"
+          event-returned (create-event-from-line event-line)]
+      (is (instance? clolog.events.Event event-returned))
+      (is (= true (compare-create-event-from-line-results event-returned event-line)))
+     )
+    )
+  )
+
+(deftest test-create-events-list-from-multiple-lines
+  (testing "Testing the creation of a list of Events from multiple lines"
+    (let [event-lines "09/04/2023,000001,activity1,joe1,event_data,data_1
+10/04/2023,000002,activity2,joe2,event_data,data_2
+11/04/2023,000003,activity3,joe3,event_data,data_3"
+          event-list-returned (create-event-list-from-multiple-lines event-lines)]
+      (is (= (count event-list-returned) 3))
+     )
+    )
   )
